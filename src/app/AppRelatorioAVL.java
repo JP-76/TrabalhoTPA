71db2fc74b54953e62a1541e6d3b8d7d489f4e5f
Erick Komati <109813173+kenzokomati@users.noreply.github.com>
2024-06-20 22:11:07 -0300

    Tirar comentario
    tirar comentario

--- src/app/AppRelatorioAVL.java
+++ src/app/AppRelatorioAVL.java
@@ -28,28 +28,28 @@ public class AppRelatorioAVL {
         gerador.geraArvoreDegenerada(100, arv);
         System.out.println("Árvore AVL Criada");
         System.out.println("Quantidade de Nós: " + arv.quantidadeNos()+ " Altura: " + arv.altura());
-        // arv = new BinaryTreeImpl(comparador);
-        // gerador.geraArvoreDegenerada(100, arv);
-        // System.out.println("Árvore Degenerada Criada");
-        // System.out.println("Quantidade de Nós: " + arv.quantidadeNos()+ " Altura: " + arv.altura());
+        arv = new BinaryTreeImpl(comparador);
+        gerador.geraArvoreDegenerada(100, arv);
+        System.out.println("Árvore Degenerada Criada");
+        System.out.println("Quantidade de Nós: " + arv.quantidadeNos()+ " Altura: " + arv.altura());
 
-        // arv = new AVLTreeImpl(comparador);
-        // gerador.geraArvoreDegenerada(1000, arv);
-        // System.out.println("Árvore AVL Criada");
-        // System.out.println("Quantidade de Nós: " + arv.quantidadeNos()+ " Altura: " + arv.altura());
-        // arv = new BinaryTreeImpl(comparador);
-        // gerador.geraArvoreDegenerada(1000, arv);
-        // System.out.println("Árvore Degenerada Criada");
-        // System.out.println("Quantidade de Nós: " + arv.quantidadeNos()+ " Altura: " + arv.altura());
+        arv = new AVLTreeImpl(comparador);
+        gerador.geraArvoreDegenerada(1000, arv);
+        System.out.println("Árvore AVL Criada");
+        System.out.println("Quantidade de Nós: " + arv.quantidadeNos()+ " Altura: " + arv.altura());
+        arv = new BinaryTreeImpl(comparador);
+        gerador.geraArvoreDegenerada(1000, arv);
+        System.out.println("Árvore Degenerada Criada");
+        System.out.println("Quantidade de Nós: " + arv.quantidadeNos()+ " Altura: " + arv.altura());
         
-        // arv = new AVLTreeImpl(comparador);
-        // gerador.geraArvoreDegenerada(10000, arv);
-        // System.out.println("Árvore AVL Criada");
-        // System.out.println("Quantidade de Nós: " + arv.quantidadeNos()+ " Altura: " + arv.altura());
-        // arv = new BinaryTreeImpl(comparador);
-        // gerador.geraArvoreDegenerada(10000, arv);
-        // System.out.println("Árvore Degenerada Criada");
-        // System.out.println("Quantidade de Nós: " + arv.quantidadeNos()+ " Altura: " + arv.altura());
+        arv = new AVLTreeImpl(comparador);
+        gerador.geraArvoreDegenerada(10000, arv);
+        System.out.println("Árvore AVL Criada");
+        System.out.println("Quantidade de Nós: " + arv.quantidadeNos()+ " Altura: " + arv.altura());
+        arv = new BinaryTreeImpl(comparador);
+        gerador.geraArvoreDegenerada(10000, arv);
+        System.out.println("Árvore Degenerada Criada");
+        System.out.println("Quantidade de Nós: " + arv.quantidadeNos()+ " Altura: " + arv.altura());
 
 
     }
